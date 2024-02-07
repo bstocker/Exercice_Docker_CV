009fed50df787deb8d069e46b23bffe91ac3bdbd
bstocker <boris.stocker@gmail.com>
2024-02-07 16:48:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,11 +4,11 @@ app = Flask(__name__) #creating flask app name
 
 @app.route('/')
 def home():
-    return render_template("resume_1.html")
-
-@app.route('/index')
-def resume_1():
     return render_template("index.html")
+
+@app.route('/resume_1')
+def resume_1():
+    return render_template("resume_1.html")
 
 @app.route('/resume_2')
 def resume_2():
